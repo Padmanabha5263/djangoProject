f56916decb2e5baf2cecc6b5c3f9fc7397facc9f
Padmanabha5263 <npadmanabha319@gmail.com>
2022-06-27 21:30:56 +0530

    updated message

--- collegeInfo/models.py
+++ collegeInfo/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class ReachUp(models.Model):
+    name = models.CharField(max_length=50)
+    email = models.CharField(max_length=60)
+    subject = models.CharField()
+    class Meta:
+        db_name = "ReachUp"
