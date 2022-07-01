717dcf8b073937555f481300481e9e7d158c6503
Padmanabha5263 <npadmanabha319@gmail.com>
2022-07-01 22:14:49 +0530

    home url redirect added

--- rljit/urls.py
+++ rljit/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include("collegeInfo.urls")),
+    path('collegeInfo/', include("collegeInfo.urls")),
+    path('',RedirectView.as_view(url="collegeInfo/"))
 ]
