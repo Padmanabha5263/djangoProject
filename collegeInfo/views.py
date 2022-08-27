8640c7a16c80b281831e89fa3647e3d35f03bda7
Padmanabha5263 <npadmanabha319@gmail.com>
2022-08-27 15:40:56 +0530

    form updated

--- collegeInfo/views.py
+++ collegeInfo/views.py
@@ -1,17 +1,25 @@
 from django.shortcuts import redirect, render
+import json
+from django.http import  JsonResponse
 from django.urls import reverse
 from .models import ReachUp
 # Create your views here.
 
 def index(request):
     if request.method == "POST":
-        name = request.POST["txtName"]
-        email = request.POST["txtEmail"]
-        subject = request.POST["txtSubject"]
-        message = request.POST['txtMessage']
-        ReachUp.objects.create(name=name, email=email, subject=subject, message=message)
-        print("success")
-        return redirect(reverse("collegeInfo:index"))
+        name = request.POST["name"]
+        email = request.POST["email"]
+        subject = request.POST["subject"]
+        message = request.POST['message']
+        try:
+            ReachUp.objects.create(name=name, email=email, subject=subject, message=message)
+            converTime ={
+                "name":name,
+                "message":"Thank you For your Query kindly we will get back to you"
+            }
+            return JsonResponse(converTime, status=200)
+        except:
+            print("error")
     else:
         return render(request, "collegeInfo/index.html")
 
